ad9f5a37f6f07e02c9f8a0bc8e0f93a119c24525
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-02-08 14:54:21 -0500

    commit

--- js/weather.js
+++ js/weather.js
@@ -6,11 +6,11 @@ document.querySelector(".navigation").classList.toggle("responsive");
 
 
 function getUpdate(){
-  
-    var dayOfWeek = new Date().getDay();
-    var month = new Date().getMonth();
-    var dayOfMonth = new Date().getDate();
-    var year = new Date().getFullYear();
+    var myday = new Date();
+    var dayOfWeek = myday.getDay();
+    var month = myday.getMonth();
+    var dayOfMonth = myday.getDate();
+    var year = myday.getFullYear();
     
 var Months = ['January', 'Febrary', 'March', 'April', 'May', 'Jun', 'July',
 'August', 'September', 'October', 'November', 'December'];
@@ -29,7 +29,7 @@ function pancakeD(){
 var dayOfWeek = new Date().getDay();
 var weekdays = ['Sunday', 'Moday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-if(weekdays[dayOfWeek] == weekdays[5]){
+if(weekdays[dayOfWeek] == weekdays[6]){
 document.getElementById("pancake").style.display = "flex";
 }
 else {
